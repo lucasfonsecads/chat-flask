348f7a17f01f447e9feac0bf166290abc9c6c1ed
Lucas <Lucas@MacBook-Air-de-MAC.local>
2017-11-19 11:28:24 -0300

    Add forms.py :smile:

---
+++ app/main/forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import Form
+from wtforms.fields import StringField, SubmitField
+from wtforms.validators import Required
+
+
+class LoginForm(Form):
+    #Enter Name and Room 
+    name = StringField('Name', validators=[Required()])
+    room = StringField('Room', validators=[Required()])
+    submit = SubmitField('Enter Chatroom')
