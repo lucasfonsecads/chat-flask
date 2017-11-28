1a099e188192e0143d163eb56652a8d11e4a2cd1
Lucas <lucafonmiranda@gmail.com>
2017-11-28 19:31:01 -0300

    Add events.py :smile:

---
+++ app/main/events.py
@@ -0,0 +1,26 @@
+from flask import session
+from flask_socketio import emit, join_room, leave_room
+from .. import socketio
+
+
+@socketio.on('joined', namespace='/chat')
+def joined(message):
+    """Sent by clients when they enter a room. """
+    room = session.get('room')
+    join_room(room)
+    emit('status', {'msg': session.get('name') + ' has entered the room.'}, room=room)
+
+
+@socketio.on('text', namespace='/chat')
+def text(message):
+    """Sent by a client when the user entered a new message."""
+    room = session.get('room')
+    emit('message', {'msg': session.get('name') + ':' + message['msg']}, room=room)
+
+
+@socketio.on('left', namespace='/chat')
+def left(message):
+    """Sent by clients when they leave a room."""
+    room = session.get('room')
+    leave_room(room)
+    emit('status', {'msg': session.get('name') + ' has left the room.'}, room=room)
