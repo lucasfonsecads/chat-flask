0c25073e4422d5cad663f0eb34599c7c442bee70
Lucas <lucasfonmiranda@gmail.com>
2017-11-29 18:24:38 -0300

    Update run.py :octocat:

--- run.py
+++ run.py
@@ -7,6 +7,7 @@ Version: 1.0.0
 from app import create_app, socketio
 
 app = create_app(debug=True)
+thread = True #Example to use thread in python flaks
 
 if __name__ == '__main__':
     socketio.run(app)
